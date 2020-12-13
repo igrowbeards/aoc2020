d95797b1fea0f95f9c76c71aef7e8c85307520d2
Justin Edwards <justin@igrowbeards.com>
2020-12-13 13:06:20 -0600

    solve day 11 part 2 - sloppy but weekend

--- lib/11.ex
+++ lib/11.ex
@@ -8,18 +8,40 @@ defmodule DayEleven do
     |> mutate_seating_until_dupe()
   end
 
+  def run_p2(input \\ @input_path) do
+    input
+    |> File.read!()
+    |> parse_input()
+    |> mutate_seating_until_dupe_picky()
+  end
+
   def parse_input(input) do
     input
     |> String.split("\n", trim: true)
-    |> Enum.map(& String.split(&1, "", trim: true))
+    |> Enum.with_index()
+    |> Enum.reduce(%{}, &parse_row/2)
+  end
+
+  def parse_row({row_string, row_index}, acc) do
+    row_string
+    |> String.split("", trim: true)
+    |> Enum.with_index()
+    |> Enum.reduce(acc, fn {seat_status, seat_index}, acc -> Map.put(acc, {row_index, seat_index}, seat_status) end)
+  end
+
+  def mutate_seating_until_dupe_picky(seating_list, seen \\ []) do
+    mutated = mutate_seating_picky(seating_list)
+    if Enum.member?(seen, mutated) do
+      Enum.count(mutated, fn {_, status} -> status == "#" end)
+    else
+      mutate_seating_until_dupe_picky(mutated, [mutated | seen])
+    end
   end
 
   def mutate_seating_until_dupe(seating_list, seen \\ []) do
     mutated = mutate_seating(seating_list)
     if Enum.member?(seen, mutated) do
-      mutated
-      |> List.flatten()
-      |> Enum.count(& &1 == "#")
+      Enum.count(mutated, fn {_, status} -> status == "#" end)
     else
       mutate_seating_until_dupe(mutated, [mutated | seen])
     end
@@ -27,64 +49,75 @@ defmodule DayEleven do
 
   def mutate_seating(seating_list) do
     seating_list
-    |> Enum.with_index()
-    |> Enum.map(fn {seats_in_row, row_number} -> 
-      seats_in_row
-      |> Enum.with_index()
-      |> Enum.map(fn {seat_status, seat_col} ->
-        surrounding = get_surrounding_seats(seating_list, row_number, seat_col)
-        mutate_seat(seat_status, surrounding)
-      end)
-    end)
+    |> Enum.map(fn {index, seat_status} -> {index, mutate_seat(seat_status, get_surrounding_seats(seating_list, index))} end)
+    |> Map.new()
   end
 
-  def get_surrounding_seats(seats, seat_row, seat_col) do
-    row_before = get_col(seats, seat_row - 1)
-
-    nw = get_seat(row_before, seat_col - 1)
-    n = get_seat(row_before, seat_col)
-    ne = get_seat(row_before, seat_col + 1)
-
-    row_of = get_col(seats, seat_row)
-    w = get_seat(row_of, seat_col - 1)
-    e = get_seat(row_of, seat_col + 1)
-
-    row_after = get_col(seats, seat_row + 1)
-
-    sw = get_seat(row_after, seat_col - 1)
-    s = get_seat(row_after, seat_col)
-    se = get_seat(row_after, seat_col + 1)
-
-    [nw, n, ne, w, e, sw, s, se]
-  end
-
-  def get_col([h | _rest] = seat_rows, index) when index >= 0 do
-    default = Enum.map(0..length(h), fn _ -> nil end)
-    Enum.at(seat_rows, index, default)
-  end
-
-  def get_col([h | _], _index) do
-    Enum.map(0..length(h), fn _ -> nil end)
-  end
-
-  def get_seat(seats, index) when index >= 0 do
-    Enum.at(seats, index, nil)
-  end
-
-  def get_seat(_seats, _index), do: nil
-
-  def parse_row(input) do
-    input
-    |> String.split("", trim: true)
-  end
-
-  def seating_list_to_string(seating_list) do
+  def mutate_seating_picky(seating_list) do
     seating_list
+    |> Enum.map(fn {index, seat_status} -> {index, mutate_seat(seat_status, get_nearest_seats(seating_list, index), true)} end)
+    |> Map.new()
+  end
+
+  def get_surrounding_seats(seats, {seat_row, seat_col}) do
+    [
+      {seat_row - 1, seat_col - 1},
+      {seat_row - 1, seat_col},
+      {seat_row - 1, seat_col + 1},
+      {seat_row, seat_col - 1},
+      {seat_row, seat_col + 1},
+      {seat_row + 1, seat_col - 1},
+      {seat_row + 1, seat_col},
+      {seat_row + 1, seat_col + 1},
+    ]
+    |> Enum.map(& Map.get(seats, &1, nil))
+  end
+
+  def get_nearest_seats(seats, seat_id) do
+    [
+      get_nearest(seats, seat_id, :nw),
+      get_nearest(seats, seat_id, :n),
+      get_nearest(seats, seat_id, :ne),
+      get_nearest(seats, seat_id, :w),
+      get_nearest(seats, seat_id, :e),
+      get_nearest(seats, seat_id, :sw),
+      get_nearest(seats, seat_id, :s),
+      get_nearest(seats, seat_id, :se)
+    ]
+  end
+
+  def offset_for(seat, dir, offset \\ 1)
+
+  def offset_for({r, c}, :nw, offset), do: {r - offset, c - offset}
+  def offset_for({r, c}, :n, offset), do: {r - offset, c}
+  def offset_for({r, c}, :ne, offset), do: {r - offset, c + offset}
+  def offset_for({r, c}, :w, offset), do: {r, c - offset}
+  def offset_for({r, c}, :e, offset), do: {r, c + offset}
+  def offset_for({r, c}, :sw, offset), do: {r + offset, c - offset}
+  def offset_for({r, c}, :s, offset), do: {r + offset, c}
+  def offset_for({r, c}, :se, offset), do: {r + offset, c + offset}
+
+  def get_nearest(seats, seat_id, dir, offset \\ 1) do
+    case Map.get(seats, offset_for(seat_id, dir, offset)) do
+      "." -> get_nearest(seats, seat_id, dir, offset + 1)
+      other -> other
+    end
+  end
+
+  def seating_list_to_string(seating_list, _room_width) do
+    room_width = Enum.count(seating_list, fn {{row_num, _}, _} -> row_num == 0 end)
+
+    seating_list
+    |> Enum.sort()
+    |> Enum.map(fn {_, status} -> status end)
+    |> Enum.chunk_every(room_width)
     |> Enum.map(& Enum.join(&1, ""))
     |> Enum.join("\n")
   end
 
-  def mutate_seat("L", surrounding_seats) do
+  def mutate_seat(status, surrounding_seats, picky \\ false)
+
+  def mutate_seat("L", surrounding_seats, _picky) do
     if Enum.all?(surrounding_seats, & Enum.member?(["L", ".", nil], &1)) do
       "#"
     else
@@ -92,7 +125,7 @@ defmodule DayEleven do
     end
   end
 
-  def mutate_seat("#", surrounding_seats) do
+  def mutate_seat("#", surrounding_seats, false) do
     if Enum.count(surrounding_seats, & &1 == "#") >= 4 do
       "L"
     else
@@ -100,5 +133,13 @@ defmodule DayEleven do
     end
   end
 
-  def mutate_seat(other, _surrounding_seats), do: other
+  def mutate_seat("#", surrounding_seats, true) do
+    if Enum.count(surrounding_seats, & &1 == "#") >= 5 do
+      "L"
+    else
+      "#"
+    end
+  end
+
+  def mutate_seat(other, _surrounding_seats, _picky), do: other
 end
--- test/day_eleven_test.exs
+++ test/day_eleven_test.exs
@@ -34,7 +34,7 @@ defmodule DayElevenTest do
       input
       |> DayEleven.parse_input()
       |> DayEleven.mutate_seating()
-      |> DayEleven.seating_list_to_string() 
+      |> DayEleven.seating_list_to_string(10)
 
     assert actual == expected
 
@@ -56,44 +56,86 @@ defmodule DayElevenTest do
       actual
       |> DayEleven.parse_input()
       |> DayEleven.mutate_seating()
-      |> DayEleven.seating_list_to_string() 
+      |> DayEleven.seating_list_to_string(10)
 
     assert actual == round_two
   end
 
+  test "get_nearest_seats" do
+    input = """
+    .......#.
+    ...#.....
+    .#.......
+    .........
+    ..#L....#
+    ....#....
+    .........
+    #........
+    ...#.....
+    """
+    |> String.trim()
+    |> DayEleven.parse_input()
+
+    assert ["#", "#", "#", "#", "#", "#", "#", "#"] = DayEleven.get_nearest_seats(input, {4, 3})
+  end
+
   # @tag :skip
   test "get_surrounding_seats" do
-    input = [
-      ["nw", "n", "ne"],
-      ["w", "target", "e"],
-      ["sw", "s", "se"]
-    ]
+    input = %{
+      {0, 0} => "nw",
+      {0, 1} => "n",
+      {0, 2} => "ne",
+      {1, 0} => "w",
+      {1, 1} => "target",
+      {1, 2} => "e",
+      {2, 0} => "sw",
+      {2, 1} => "s",
+      {2, 2} => "se"
+    }
 
-    assert ["nw", "n", "ne", "w", "e", "sw", "s", "se"] = DayEleven.get_surrounding_seats(input, 1, 1)
+    assert ["nw", "n", "ne", "w", "e", "sw", "s", "se"] = DayEleven.get_surrounding_seats(input, {1, 1})
 
-    input = [
-      ["target", "e",  "not_used_a"],
-      ["s",      "se", "not_used_b"],
-      ["not_used_c",      "not_used_d",  "not_used_e"]
-    ]
+    input = %{
+      {0, 0} => "target",
+      {0, 1} => "e",
+      {0, 2} => "not_used_a",
+      {1, 0} => "s",
+      {1, 1} => "se",
+      {1, 2} => "not_used_b",
+      {2, 0} => "not_used_c",
+      {2, 1} => "not_used_d",
+      {2, 2} => "not_used_e"
+    }
 
-    assert [nil, nil, nil, nil, "e", nil, "s", "se"] = DayEleven.get_surrounding_seats(input, 0, 0)
+    assert [nil, nil, nil, nil, "e", nil, "s", "se"] = DayEleven.get_surrounding_seats(input, {0, 0})
 
-    input = [
-      ["", "", ""],
-      ["", "nw", "n"],
-      ["", "w", "target"]
-    ]
+    input = %{
+      {0, 0} => "",
+      {0, 1} => "",
+      {0, 2} => "",
+      {1, 0} => "",
+      {1, 1} => "nw",
+      {1, 2} => "n",
+      {2, 0} => "",
+      {2, 1} => "w",
+      {2, 2} => "target"
+    }
 
-    assert ["nw", "n", nil, "w", nil, nil, nil, nil] = DayEleven.get_surrounding_seats(input, 2, 2)
+    assert ["nw", "n", nil, "w", nil, nil, nil, nil] = DayEleven.get_surrounding_seats(input, {2, 2})
 
-    input = [
-      ["", "", ""],
-      ["n", "ne", ""],
-      ["target", "e", ""]
-    ]
+    input = %{
+      {0, 0} => "",
+      {0, 1} => "",
+      {0, 2} => "",
+      {1, 0} => "n",
+      {1, 1} => "ne",
+      {1, 2} => "",
+      {2, 0} => "target",
+      {2, 1} => "e",
+      {2, 2} => ""
+    }
 
-    assert [nil, "n", "ne", nil, "e", nil, nil, nil] = DayEleven.get_surrounding_seats(input, 2, 0)
+    assert [nil, "n", "ne", nil, "e", nil, nil, nil] = DayEleven.get_surrounding_seats(input, {2, 0})
   end
 
   # @tag :skip
@@ -101,5 +143,47 @@ defmodule DayElevenTest do
     assert DayEleven.mutate_seat("L", ["L", "L", "L", "L", "L", "L", "L", "L"]) == "#"
 
     assert DayEleven.mutate_seat("#", ["#", "#", "#", "#", "L", "L", "L", "L"]) == "L"
+  end
+
+  # @tag :skip
+  test "mutate until dupe" do
+    results = """
+    L.LL.LL.LL
+    LLLLLLL.LL
+    L.L.L..L..
+    LLLL.LL.LL
+    L.LL.LL.LL
+    L.LLLLL.LL
+    ..L.L.....
+    LLLLLLLLLL
+    L.LLLLLL.L
+    L.LLLLL.LL
+    """
+    |> String.trim()
+    |> DayEleven.parse_input()
+    |> DayEleven.mutate_seating_until_dupe()
+
+    assert results == 37
+  end
+
+  # @tag :skip
+  test "mutate until dupe picky" do
+    results = """
+    L.LL.LL.LL
+    LLLLLLL.LL
+    L.L.L..L..
+    LLLL.LL.LL
+    L.LL.LL.LL
+    L.LLLLL.LL
+    ..L.L.....
+    LLLLLLLLLL
+    L.LLLLLL.L
+    L.LLLLL.LL
+    """
+    |> String.trim()
+    |> DayEleven.parse_input()
+    |> DayEleven.mutate_seating_until_dupe_picky()
+
+    assert results == 26
   end
 end
